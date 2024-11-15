3e19c1d6dff5f4d07eb814d5f95dfcf41844e5b2
ElsincejasWasaaaaa <guidoneira6@gmail.com>
2024-11-15 00:09:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+df = pd.read_csv('https://raw.githubusercontent.com/ElsincejasWasaaaaa/data/refs/heads/main/Tabladerendimientoacademico1.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/ElsincejasWasaaaaa/data/refs/heads/main/ComparativaDeSatisfaccionEstudiantil1.csv')
+
 def main():
     st.title("DataViz")
     st.header("Dataframe:")
     st.info("Estamos probando cosas! todavia en mantenimiento")
+    st.dataframe(df)
+    st.dataframe(df1)
 
 if __name__ == "__main__":
     main()
