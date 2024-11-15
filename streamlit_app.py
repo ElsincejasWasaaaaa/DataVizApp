8341e35d2a9670c811acb3562cebcc76adb306f9
ElsincejasWasaaaaa <guidoneira6@gmail.com>
2024-11-14 23:56:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 def main():
     st.title("DataViz")
     st.header("Dataframe:")
+    st.info("Estamos probando cosas! todavia en mantenimiento)
 
 if __name__ == "__main__":
     main()
